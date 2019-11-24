e6508c2d56d4444df63c30c9e7479b0000acc9e6
Shvejan <shvejan2011@gmail.com>
2019-11-25 05:17:53 +0530

    login,logout

--- app1.py
+++ app1.py
@@ -1,28 +1,57 @@
 import os
-from flask import Flask, session,render_template
-from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-
+from flask import Flask,session,render_template,request,redirect,url_for
+from classes.booksusers import *
 
 app = Flask(__name__)
-
-if not os.getenv('DATABASE_URL'):
-    raise RuntimeError('db not set')
-
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL2")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+app.secret_key = os.urandom(12)
 
 @app.route('/')
 def home():
+    if session.get('logged_in'):
+        return render_template('welcome.html')
     return render_template('home.html')
-@app.route('/login')
+
+@app.route('/login',methods=['POST','GET'])
 def login():
-    pass
-@app.route('/signup')
+    if request.method == "GET":
+        return redirect(url_for('home'))
+    if session.get('logged_in'):
+        return redirect(url_for('home'))
+    else:
+        n=request.form.get('lname')
+        p=request.form.get('lpass')
+        x = users.query.filter_by(username=n).first()
+        if(x.checkPass):
+            session['logged_in'] = True
+        else:
+            flash('wrong password!')
+    return redirect(url_for('home'))
+
+
+
+@app.route('/signup',methods=['POST','GET'])
 def signup():
-    pass
+    if request.method == "GET":
+        return redirect(url_for('home'))
+    if session.get('logged_in'):
+        return redirect(url_for('home'))
+    else:
+
+        n=request.form.get('sname')
+        p=request.form.get('spass')
+        e=request.form.get('semail')            
+        x=users(username=n,password=p,email=e)
+        db.session.add(x)
+        db.session.commit()
+        session['logged_in'] = True
+    return redirect(url_for('home'))
+
+@app.route('/logout',methods=['POST','GET'])
+def logout():
+    if request.method == "GET":
+        return redirect(url_for('home'))
+    session['logged_in']=False
+    return render_template('home.html')
--- classes/booksusers.py
+++ classes/booksusers.py
@@ -13,6 +13,11 @@ class books(db.Model):
 class users(db.Model):
     __tablename__='users'
     id = db.Column(db.Integer,primary_key='true')
-    username = db.Column(db.String,nullable=False)
+    username = db.Column(db.String,nullable=False,unique = True)
     password = db.Column(db.String,nullable=False)
-    email = db.Column(db.String,nullable=False)
+    email = db.Column(db.String,nullable=False,unique = True)
+
+    def checkPass(a):
+        if password==a:
+            return True
+        return False
