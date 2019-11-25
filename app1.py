60c16a64100fbdb527e2fc203f3a5052cf173b6c
Shvejan <shvejan2011@gmail.com>
2019-11-25 22:07:42 +0530

    all done

--- app1.py
+++ app1.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask,session,render_template,request,redirect,url_for
+from flask import Flask,session,render_template,request,redirect,url_for,flash
 from classes.booksusers import *
+import requests
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL2")
@@ -23,9 +24,11 @@ def login():
     else:
         n=request.form.get('lname')
         p=request.form.get('lpass')
-        x = users.query.filter_by(username=n).first()
-        if(x.checkPass):
+        cur_user = users.query.filter_by(username=n).first()
+        if(cur_user.checkPass(p)):
             session['logged_in'] = True
+            session['user']=n
+
         else:
             flash('wrong password!')
     return redirect(url_for('home'))
@@ -42,11 +45,12 @@ def signup():
 
         n=request.form.get('sname')
         p=request.form.get('spass')
-        e=request.form.get('semail')            
-        x=users(username=n,password=p,email=e)
-        db.session.add(x)
+        e=request.form.get('semail')
+        cur_user=users(username=n,password=p,email=e)
+        db.session.add(cur_user)
         db.session.commit()
         session['logged_in'] = True
+        session['user']=n
     return redirect(url_for('home'))
 
 @app.route('/logout',methods=['POST','GET'])
@@ -55,3 +59,45 @@ def logout():
         return redirect(url_for('home'))
     session['logged_in']=False
     return render_template('home.html')
+
+@app.route('/results',methods=['POST'])
+def results():
+    exp = request.form.get('exp')
+    p = books.query.all()
+    x=[]
+    for a in p:
+        if a.match(exp):
+            x.append(a)
+
+    return render_template('results.html',x=x)
+
+@app.route('/open/<string:var>',methods=['POST','GET'])
+def open(var):
+    b=books.query.filter_by(isbn=var).first()
+
+    u = users.query.filter_by(username=session.get('user')).first()
+    if request.method=='POST':
+        r=reviews(msg = request.form.get('rev'), user_id=u.id, book_id=b.id)
+        db.session.add(r)
+        db.session.commit()
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "j9nL5Qi4UvfI66ErTe0cHw", "isbns": var})
+    all_rev = reviews.query.filter_by(book_id=b.id).all()
+    d={}
+    for x in all_rev:
+        tu=users.query.filter_by(id=x.user_id).first()
+        d[tu.username]=x.msg
+    return render_template('open.html',  x=res.json(),b=b,all_rev = d )
+
+@app.route('/api/<string:id>')
+def api(id):
+    x=books.query.filter_by(isbn=id).first()
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "j9nL5Qi4UvfI66ErTe0cHw", "isbns": id})
+    res=res.json()
+    return {
+    "title": x.title,
+    "author": x.author,
+    "year": x.year,
+    "isbn": x.isbn,
+    "review_count": res['books'][0]["reviews_count"],
+    "average_score": res['books'][0]["average_rating"]
+    }
--- classes/booksusers.py
+++ classes/booksusers.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-
+import re
 db=SQLAlchemy()
 
 class books(db.Model):
@@ -10,6 +10,18 @@ class books(db.Model):
     author=db.Column(db.String,nullable=False)
     year=db.Column(db.Integer,nullable=False)
 
+    def display(self):
+        print(self.isbn , self.title, self.author, self.year)
+    def match(self,x):
+        if re.search(x,self.isbn):
+            return True
+        if re.search(x,self.title) or re.search(x.capitalize(),self.title):
+            return True
+        if re.search(x,self.author) or re.search(x.capitalize(),self.author):
+            return True
+        return False
+
+
 class users(db.Model):
     __tablename__='users'
     id = db.Column(db.Integer,primary_key='true')
@@ -17,7 +29,14 @@ class users(db.Model):
     password = db.Column(db.String,nullable=False)
     email = db.Column(db.String,nullable=False,unique = True)
 
-    def checkPass(a):
-        if password==a:
+    def checkPass(self,a):
+        if self.password==a:
             return True
         return False
+
+class reviews(db.Model):
+    __tablename__='reviews'
+    id = db.Column(db.Integer,primary_key='true')
+    msg = db.Column(db.String,nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"),nullable=False)
+    book_id = db.Column(db.Integer, db.ForeignKey("books.id"),nullable=False)
